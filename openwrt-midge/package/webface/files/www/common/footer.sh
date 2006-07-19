bfb6c7f02aa3fb919a70de39575c7dcf740db4ea
Vlad Moskovets <devvlad@gmail.com>
2006-07-19 12:25:08 +0000

    git-svn-id: http://midge.vlad.org.ua/svn/trunk@77 9f4ba3df-1318-0410-9984-8b9d03078064

--- openwrt-midge/package/webface/files/www/common/footer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# (c) Vladislav Moskovets 2005
-# Sigrand webface project
-#
-
-echo '</div>
-</body></html>'
--- openwrt-midge/package/webface/files/www/welcome.asp
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/haserl
-<? . common/header.sh ?>
-
-Welcome to modem of Sigrand!
-
-<? . common/footer.sh ?>
