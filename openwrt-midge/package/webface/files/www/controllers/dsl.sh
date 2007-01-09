ef49f21246930b0e6faf288bf92c26492528b7a8
Vlad Moskovets <devvlad@gmail.com>
2007-01-09 16:21:23 +0000

    webface: art's dsl patch
    git-svn-id: http://midge.vlad.org.ua/svn/trunk@423 9f4ba3df-1318-0410-9984-8b9d03078064

--- openwrt-midge/package/webface/files/www/controllers/dsl.sh
+++ openwrt-midge/package/webface/files/www/controllers/dsl.sh
@@ -2,6 +2,8 @@
 	. conf/conf.sh
 	. lib/misc.sh
 	. lib/widgets.sh
+
+cfg_path="/sys/bus/pci/drivers/sg16lan"
 	
 print_dsl_settings(){
 	
@@ -10,30 +12,43 @@ print_dsl_settings(){
 	local var2
 	
 	iface="$1"
-        render_table_title $iface" dsl interface sttings" 2
+
+	
+        render_table_title $iface" interface sttings" 2
 
 	# hidden mark dsl_iface_name
 	tip=""
 	desc=""
 	validator='tmt:message="'$desc'"'
-	var2="dsl_iface_name"	
+	var2="iface"	
 	render_input_field "hidden" "hidden" $var2 $iface
+
+
+	# hidden mark dsl_iface_name
+	link=`cat ${cfg_path}/${iface}/state`
+	tip=""
+	desc="Link state"
+	validator='tmt:message="'$desc'"'
+	var1="link state"		
+	var2="link"	
+#	render_input_field select "$var1" $var2 online online offline offline
+	render_2table_field "$var1" "$link"
 
 	
 	# sys_dsl_ethX_rate
 	tip=""
 	desc="Select DSL line rate"
 	validator='tmt:message="'$desc'"'
-	var1=$iface" rate"
-	var2="sys_dsl_"$iface"_rate"	
+	var1="rate"
+	var2="sys_dsl_${iface}_rate"	
 	render_input_field text "$var1" $var2  
 
 	# sys_dsl_ethX_mode
 	tip=""
 	desc="Select DSL mode"
 	validator='tmt:message="'$desc'"'
-	var1=$iface" mode"	
-	var2="sys_dsl_"$iface"_mode"	
+	var1="mode"	
+	var2="sys_dsl_${iface}_mode"		
 	render_input_field select "$var1" $var2 master master slave slave
 
 
@@ -41,32 +56,32 @@ print_dsl_settings(){
 	tip=""
 	desc="Select DSL line coding"
 	validator='tmt:message="'$desc'"'
-	var1=$iface" coding"	
-	var2="sys_dsl_"$iface"_code"	
+	var1="coding"	
+	var2="sys_dsl_${iface}_coding"
 	render_input_field select "$var1" $var2 tcpam32 TCPAM32 tcpam16 TCPAM16 tcpam8 TCPAM8 tcpam4 TCPAM4
 
 	# sys_dsl_ethX_cfg
 	tip=""
 	desc="Select DSL configuration mode"
 	validator='tmt:message="'$desc'"'
-	var1=$iface" config"	
-	var2="sys_dsl_"$iface"_cfg"	
+	var1="config"	
+	var2="sys_dsl_${iface}_cfg"	
 	render_input_field select "$var1" $var2 local local preact preact
 
 	# sys_dsl_ethX_annex
 	tip=""
 	desc="Select DSL Annex"
 	validator='tmt:message="'$desc'"'
-	var1=$iface" annex"	
-	var2="sys_dsl_"$iface"_annex"	
+	var1="annex"	
+	var2="sys_dsl_${iface}_annex"	
 	render_input_field select "$var1" $var2 A "Annex A" B "Annex B" F "Annex F"
 
 	# sys_dsl_ethX_mac
 	tip=""
 	desc="Select Ethernet MAC address(last 3 numbers)"
 	validator='tmt:message="'$desc'"'	
-	var1=$iface" MAC"	
-	var2="sys_dsl_"$iface"_mac"	
+	var1="MAC"	
+	var2="sys_dsl_${iface}_mac"	
 	render_input_field text "$var1" $var2 
 
 
@@ -74,53 +89,51 @@ print_dsl_settings(){
 	tip=""
 	desc="Select DSL CRC length"
 	validator='tmt:message="'$desc'"'
-	var1=$iface" CRC"	
-	var2="sys_dsl_"$iface"_crc32"	
+	var1="CRC"	
+	var2="sys_dsl_${iface}_crc32"	
 	render_input_field select "$var1" $var2 crc32 CRC32 crc16 CRC16
 
 	# sys_dsl_ethX_fill
 	tip=""
 	desc="Select DSL fill byte value"
 	validator='tmt:message="'$desc'"'
-	var1=$iface" fill"	
-	var2="sys_dsl_"$iface"_fill"	
+	var1="fill"	
+	var2="sys_dsl_${iface}_fill"	
 	render_input_field select "$var1" $var2 fill_ff FF fill_7e 7E
 
 	# sys_dsl_ethX_inv
 	tip=""
 	desc="Select DSL data inversion"
 	validator='tmt:message="'$desc'"'	
-	var1=$iface" inverse"	
-	var2="sys_dsl_"$iface"_inv"	
+	var1="inverse"	
+	var2="sys_dsl_${iface}_inv"	
 	render_input_field select "$var1" $var2 normal Normal invert Inverse
 
 }	
+
+ 
+#	render_title "SHDSL settings"	
+	[ -d "$cfg_path" ] || die "Service stopped"
 	
 	if [ $REQUEST_METHOD = POST ]; then
 		#TODO: Check save
-		kdb_vars=""
-		var="\$FORM_dsl_iface_name"
+		var="\$FORM_iface"
 		iface="`eval echo $var`"
-		echo "<br>iface="$iface
-					
-		kdb_vars="$kdb_vars sys_dsl_${iface}_rate sys_dsl_${iface}_mode sys_dsl_${iface}_code \
-				sys_dsl_${iface}_cfg sys_dsl_${iface}_mac\
-			sys_dsl_${iface}_annex sys_dsl_${iface}_crc32 \ 
-			sys_dsl_${iface}_fill sys_dsl_${iface}_inv"
 		
-		if [ "$KDB_ERROR" ]; then
-			render_message_box "Error" "Savings failed: $KDB_ERROR"
-		else
-			render_message_box "Done" "Settings saved"
-		fi
-			subsys="dsl."$iface
-		save "$kdb_vars" "$subsys"
+		kdb_vars="  int:sys_dsl_${iface}_rate	\
+			    str:sys_dsl_${iface}_mode	\
+			    str:sys_dsl_${iface}_cfg	\
+			    str:sys_dsl_${iface}_mac	\
+		            str:sys_dsl_${iface}_annex	\
+			    str:sys_dsl_${iface}_crc32	\
+		            str:sys_dsl_${iface}_fill	\
+			    str:sys_dsl_${iface}_inv"	
+		subsys="dsl."$iface
+		save  "$subsys" "$kdb_vars"
+		render_save_message		
 	fi
 
-	render_save_message
-
-	local iface
-	eval `$kdb -qq list sys_`
+	eval `$kdb -qq list sys_dsl_`
 	for iface in `kdb get sys_dsl_ifaces`; do	
 	    render_form_header dsl dsl_save.asp
 	    print_dsl_settings $iface
