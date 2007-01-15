edd71b431d74f7f70f13268db110996af012b9ff
Vlad Moskovets <devvlad@gmail.com>
2007-01-15 07:03:48 +0000

    webface: dsl mini-update
    git-svn-id: http://midge.vlad.org.ua/svn/trunk@438 9f4ba3df-1318-0410-9984-8b9d03078064

--- openwrt-midge/package/webface/files/www/controllers/dsl.sh
+++ openwrt-midge/package/webface/files/www/controllers/dsl.sh
@@ -30,37 +30,37 @@ print_dsl_settings(){
 	validator='tmt:message="'$desc'"'
 	render_input_field select "Rate" sys_dsl_${iface}_rate $(for i in `seq 1 64`; do echo $(($i*64)) $(($i*64)); done)
 
-	# sys_dsl_ethX_mode
+	# sys_dsl_${iface}_mode
 	tip=""
 	desc="Select DSL mode"
 	render_input_field select "Mode" sys_dsl_${iface}_rate  master 'Master' slave 'Slave'
 
-	# sys_dsl_ethX_code
+	# sys_dsl_${iface}_code
 	tip=""
 	desc="Select DSL line coding"
 	render_input_field select "Coding" sys_dsl_${iface}_code tcpam32 TCPAM32 tcpam16 TCPAM16 tcpam8 TCPAM8 tcpam4 TCPAM4
 
-	# sys_dsl_ethX_cfg
+	# sys_dsl_${iface}_cfg
 	tip=""
 	desc="Select DSL configuration mode"
 	render_input_field select "Config" sys_dsl_${iface}_cfg local local preact preact
 
-	# sys_dsl_ethX_annex
+	# sys_dsl_${iface}_annex
 	tip=""
 	desc="Select DSL Annex"
 	render_input_field select "Annex" sys_dsl_${iface}_annex A "Annex A" B "Annex B" F "Annex F"
 
-	# sys_dsl_ethX_crc32
+	# sys_dsl_${iface}_crc32
 	tip=""
 	desc="Select DSL CRC length"
 	render_input_field select "CRC" sys_dsl_${iface}_crc crc32 CRC32 crc16 CRC16
 
-	# sys_dsl_ethX_fill
+	# sys_dsl_${iface}_fill
 	tip=""
 	desc="Select DSL fill byte value"
 	render_input_field select "Fill" sys_dsl_${iface}_fill  fill_ff FF fill_7e 7E
 
-	# sys_dsl_ethX_inv
+	# sys_dsl_${iface}_inv
 	default=0
 	tip=""
 	desc="Select DSL data inversion"
