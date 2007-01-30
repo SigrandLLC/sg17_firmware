2358c31b9c5740818b475182d613d3f4435c6cad
Vlad Moskovets <devvlad@gmail.com>
2007-01-30 08:05:40 +0000

    webface: hdb update
    git-svn-id: http://midge.vlad.org.ua/svn/trunk@474 9f4ba3df-1318-0410-9984-8b9d03078064

--- openwrt-midge/package/webface/hdb.cpp
+++ openwrt-midge/package/webface/hdb.cpp
@@ -41,6 +41,11 @@ inline void _debug(char* format, ...){}
 //        r-r-r-r
 //      
 
+char *str_escape(const char *source);
+char *str_unescape(const char *source);
+int match_wildcard(const char *pattern, const char *string);
+int is_wildcarded(const char* str);
+
 class node {
 	node *root;
 	node *fchild;
@@ -93,6 +98,28 @@ public:
 	node *get_prev() { return prev; }
 	node *get_root() { return root; }
 	node *get_fchild() { return fchild; }
+	
+	bool parse_pair(const char* str) {
+		int len = strlen(str);
+		if (!len)
+			return false;
+		char s[len+10];
+
+		strcpy(s, str);
+		if (s[len-1]=='\n')
+			s[len-1]='\0';
+
+		char *eq=strchr(s, '=');
+		if (!eq || eq==s) {
+			fprintf(stderr, "Error: can't parse: %s", s);
+			return false;
+		}
+		eq[0]='\0';
+		strcpy(name, s );
+		strcpy(data, eq+1);
+
+		return true;
+	}
 
 	node *get_first() {
 		debug("node[%x]::get_first()\n", this);
@@ -111,18 +138,18 @@ public:
 		return next->get_last();
 	}
 
-	node *find_name(const char* str, int level=INT_MAX) {
-		debug("node[%x]::find_name(%s, %d)\n", this, str, level);
+	node *find_name(const char* str, int maxlevel=INT_MAX) {
+		debug("node[%x]::find_name(%s, %d)\n", this, str, maxlevel);
 		node *found=NULL;
 		if ( ! strcmp(str, name) )
 			return this;
 
-		if ( level <= 0 )
+		if ( maxlevel <= 0 )
 			return NULL;
 
 		node *n=fchild;
 		while ( n ) {
-			found=n->find_name(str, level-1);
+			found=n->find_name(str, maxlevel-1);
 			if ( found )
 				return found;
 			n=n->get_next();
@@ -189,6 +216,35 @@ public:
 		return add_child(new node(this));
 	}	
 
+	void serialize(FILE *stream=stdout, int level=0) {
+		int i;
+		for ( i=0; i < level; i++ )
+			fprintf(stream, "\t");
+		fprintf(stream, "%s=%s\n", str_escape(name), str_escape(data));
+
+		node *c=fchild;
+		while ( c ) {
+			c->serialize(stream, level+1);
+			c=c->get_next();
+		}
+	}
+
+	void unserialize(FILE *stream=stdin, int level=0) {
+		char *buf=NULL;
+		size_t strsize=0;
+
+		if ( getline( &buf, &strsize, stream ) != -1 ) {
+			if ( parse_pair( buf ) ) {
+				;
+			};
+
+
+			free(buf);
+		}
+
+
+	}
+
 	void dump(int level=0) {
 		for (int i=0; i < level; i++)
 			printf("\t");
@@ -232,5 +288,161 @@ int main ()
 	if (n) 
 		n->dump();
 
+	printf("serialize:\n");
+	root->serialize(stdout);
+
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// copyright (c) 2003-2005 chisel <storlek@chisel.cjb.net>
+/* adapted from glib. in addition to the normal c escapes, this also escapes the comment character (#)
+ *  * as \043.  */
+char *str_escape(const char *source)
+{
+	const char *p = source;
+	/* Each source byte needs maximally four destination chars (\777) */
+	char *dest = (char*)calloc(4 * strlen(source) + 1, sizeof(char));
+	char *q = dest;
+
+	while (*p) {
+		switch (*p) {
+		case '\a':
+			*q++ = '\\';
+			*q++ = 'a';
+		case '\b':
+			*q++ = '\\';
+			*q++ = 'b';
+			break;
+		case '\f':
+			*q++ = '\\';
+			*q++ = 'f';
+			break;
+		case '\n':
+			*q++ = '\\';
+			*q++ = 'n';
+			break;
+		case '\r':
+			*q++ = '\\';
+			*q++ = 'r';
+			break;
+		case '\t':
+			*q++ = '\\';
+			*q++ = 't';
+			break;
+		case '\v':
+			*q++ = '\\';
+			*q++ = 'v';
+			break;
+		case '\\': case '"': case '\'': 
+			*q++ = '\\';
+			*q++ = *p;
+			break;
+		default:
+			if ((*p <= ' ') || (*p >= 0177) || (*p == '=') || (*p == '#')) {
+				*q++ = '\\';
+				*q++ = '0' + (((*p) >> 6) & 07);
+				*q++ = '0' + (((*p) >> 3) & 07);
+				*q++ = '0' + ((*p) & 07);
+			} else {
+				*q++ = *p;
+			}
+			break;
+		}
+		p++;
+	}
+
+	*q = 0;
+
+	return dest;
+}
+
+/* opposite of str_escape. (this is glib's 'compress' function renamed more clearly)
+ * TODO: it'd be nice to handle \xNN as well... */
+char *str_unescape(const char *source)
+{
+	const char *p = source;
+	const char *octal;
+	char *dest = (char*)calloc(strlen(source) + 1, sizeof(char));
+	char *q = dest;
+
+	while (*p) {
+		if (*p == '\\') {
+			p++;
+			switch (*p) {
+			case '0'...'7':
+				*q = 0;
+				octal = p;
+				while ((p < octal + 3) && (*p >= '0') && (*p <= '7')) {
+					*q = (*q * 8) + (*p - '0');
+					p++;
+				}
+				q++;
+				p--;
+				break;
+			case 'a':
+				*q++ = '\a';
+				break;
+			case 'b':
+				*q++ = '\b';
+				break;
+			case 'f':
+				*q++ = '\f';
+				break;
+			case 'n':
+				*q++ = '\n';
+				break;
+			case 'r':
+				*q++ = '\r';
+				break;
+			case 't':
+				*q++ = '\t';
+				break;
+			case 'v':
+				*q++ = '\v';
+				break;
+			default:		/* Also handles \" and \\ */
+				*q++ = *p;
+				break;
+			}
+		} else {
+			*q++ = *p;
+		}
+		p++;
+	}
+	*q = 0;
+
+	return dest;
+}
+
