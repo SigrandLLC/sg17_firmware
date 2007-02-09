c972c44d511ef94af79d2781cc9c1f182a4dd388
Vlad Moskovets <devvlad@gmail.com>
2007-02-09 06:59:54 +0000

    webface: adm5120sw fixes
    git-svn-id: http://midge.vlad.org.ua/svn/trunk@485 9f4ba3df-1318-0410-9984-8b9d03078064

--- openwrt-midge/package/webface/files/www/controllers/ifaces.sh
+++ openwrt-midge/package/webface/files/www/controllers/ifaces.sh
@@ -4,7 +4,7 @@
 	del_iface=$FORM_del_iface
 
 	eval `kdb -qq ls sys_ifaces`
-	
+
 	get_next_iface() {
 		local n=0
 		local name
@@ -14,7 +14,7 @@
 			bonding)	name=bond;;
 			bridge)		name=br;;
 		esac
-			
+
 		while [ 1 ]; do
 			notfound=y
 			for i in $sys_ifaces; do
@@ -28,16 +28,15 @@
 			fi
 		done
 	}
-	
+
 	if [ $REQUEST_METHOD = POST ]; then
 		if [ -n "$iface_proto" ]; then
 			iface=`get_next_iface $iface_proto`
-			kdb set sys_ifaces="$sys_ifaces $iface" : set sys_iface_${iface}_proto=$iface_proto : set sys_iface_${iface}_real=$iface : set sys_iface_${iface}_method=none
+			iface_add $iface
 			ok_str="Interface <b>$iface</b> added, please reload page"
 			render_save_message
 		elif [ -n "$del_iface" ]; then
-			sys_ifaces=`echo $sys_ifaces | sed s/$del_iface//`
-			kdb set sys_ifaces="$sys_ifaces"
+			iface_del $del_iface
 			del_iface=""
 			ok_str="Interface deleted, please reload page"
 			render_save_message
@@ -45,8 +44,8 @@
 	fi
 
 	# Add interface
-	render_form_header ifaces 
-	render_table_title "Add dynamic interface" 2 
+	render_form_header ifaces
+	render_table_title "Add dynamic interface" 2
 
 	desc="Please select interface protocol"
 	validator='tmt:invalidindex=0 tmt:message="Please select protocol"'
@@ -56,13 +55,13 @@
 	render_form_tail
 
 	# Delete interface
-	render_form_header ifaces 
-	render_table_title "Delete dynamic interface" 2 
+	render_form_header ifaces
+	render_table_title "Delete dynamic interface" 2
 
 	desc="Please select interface"
 	validator='tmt:invalidindex=0 tmt:message="Please select interface"'
 	params=""
-	for i in $sys_ifaces; do 
+	for i in $sys_ifaces; do
 		case $i in
 			eth*|dsl*)	;;
 			*)		params="$params $i $i";;
