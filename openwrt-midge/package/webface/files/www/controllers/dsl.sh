022816597a4b30cfe6f7d1b4037c43818cdc3cbd
Artem Y. Polyakov <artpol84@gmail.com>
2007-04-26 10:37:56 +0000

    Add Dynamic option dependences in DSL web configuration Known bugs: 1. Work fine in Opera & Mozilla, but IE brokes down while loading     DSL configuration page. Bug is under fixing.
    git-svn-id: http://midge.vlad.org.ua/svn/trunk@656 9f4ba3df-1318-0410-9984-8b9d03078064

--- openwrt-midge/package/webface/files/www/controllers/dsl.sh
+++ openwrt-midge/package/webface/files/www/controllers/dsl.sh
@@ -9,7 +9,6 @@ iface="${FORM_iface}"
 	
 eval `kdb -qq ls "sys_dsl_*" `
 	
-tmp_rates="64 64 128 128 192 192 256 256 320 320 384 384 448 448 512 512 576 576 640 640 704 704 768 768 832 832 896 896 960 960 1024 1024 1088 1088 1152 1152 1216 1216 1280 1280 1344 1344 1408 1408 1472 1472 1536 1536 1600 1600 1664 1664 1728 1728 1792 1792 1856 1856 1920 1920 1984 1984 2048 2048 2112 2112 2176 2176 2240 2240 2304 2304 2368 2368 2432 2432 2496 2496 2560 2560 2624 2624 2688 2688 2752 2752 2816 2816 2880 2880 2944 2944 3008 3008 3072 3072 3136 3136 3200 3200 3264 3264 3328 3328 3392 3392 3456 3456 3520 3520 3584 3584 3648 3648 3712 3712 3776 3776 3840 3840 3904 3904 3968 3968 4032 4032 4096 4096 4160 4160 4224 4224 4288 4288 4352 4352 4416 4416 4480 4480 4544 4544 4608 4608 4672 4672 4736 4736 4800 4800 4864 4864 4928 4928 4992 4992 5056 5056 5120 5120 5184 5184 5248 5248 5312 5312 5376 5376 5440 5440 5504 5504 5568 5568 5632 5632 5696 5696 5760 5760 5824 5824 5888 5888 5952 5952 6016 6016"
 
 if [ -z "$iface" ]; then
 	render_table_title "Modem status" 2
@@ -33,40 +32,50 @@ else
 
 	render_save_stuff
 
-	render_form_header
+	render_form_header 
+	#"midge_form" "OnChangeDSLCode()"
 	render_table_title "$iface modem settings" 2
 
 	# sys_dsl_${iface}_name
 	render_input_field "hidden" "hidden" iface $iface
 
-	# TODO: static list?
+
+	tmp_rates="64 64 128 128 192 192 256 256 320 320 384 384 448 u448 512 512 576 576 640 640 704 704 768 768 832 832 896 896 960 960 1024 1024 1088 1088 1152 1152 1216 1216 1280 1280 1344 1344 1408 1408 1472 1472 1536 1536 1600 1600 1664 1664 1728 1728 1792 1792 1856 1856 1920 1920 1984 1984 2048 2048 2112 2112 2176 2176 2240 2240 2304 2304 2368 2368 2432 2432 2496 2496 2560 2560 2624 2624 2688 2688 2752 2752 2816 2816 2880 2880 2944 2944 3008 3008 3072 3072 3136 3136 3200 3200 3264 3264 3328 3328 3392 3392 3456 3456 3520 3520 3584 3584 3648 3648 3712 3712 3776 3776 3840 3840 3904 3904 3968 3968 4032 4032 4096 4096 4160 4160 4224 4224 4288 4288 4352 4352 4416 4416 4480 4480 4544 4544 4608 4608 4672 4672 4736 4736 4800 4800 4864 4864 4928 4928 4992 4992 5056 5056 5120 5120 5184 5184 5248 5248 5312 5312 5376 5376 5440 5440 5504 5504 5568 5568 5632 5632 5696 5696 5760 5760 5824 5824 5888 5888 5952 5952 6016 6016"
+	
 	# sys_dsl_${iface}_rate
 	tip=""
 	desc="Select DSL line rate"
-	id='rate'
 	validator='tmt:message="'$desc'"'
+	id='rate'
+	onchange="OnChangeDSLCode();"	
 	render_input_field select "Rate" sys_dsl_${iface}_rate $tmp_rates
 
 	# sys_dsl_${iface}_mode
 	tip=""
 	desc="Select DSL mode"
+	id='mode'
+	onchange="OnChangeDSLCode();"	
 	render_input_field select "Mode" sys_dsl_${iface}_mode  master 'Master' slave 'Slave'
 
 	# sys_dsl_${iface}_code
 	tip=""
 	desc="Select DSL line coding"
-	id=code
+	id='code'
 	onchange="OnChangeDSLCode();"
 	render_input_field select "Coding" sys_dsl_${iface}_code tcpam32 TCPAM32 tcpam16 TCPAM16 tcpam8 TCPAM8 tcpam4 TCPAM4
 
 	# sys_dsl_${iface}_cfg
 	tip=""
 	desc="Select DSL configuration mode"
+	id='cfg'
+	onchange="OnChangeDSLCode();"	
 	render_input_field select "Config" sys_dsl_${iface}_cfg local local preact preact
 
 	# sys_dsl_${iface}_annex
 	tip=""
 	desc="Select DSL Annex"
+	id='annex'
+	onchange="OnChangeDSLCode();"	
 	render_input_field select "Annex" sys_dsl_${iface}_annex A "Annex A" B "Annex B" F "Annex F"
 
 	# sys_dsl_${iface}_crc32
@@ -92,4 +101,4 @@ else
 fi
 
 
-# vim:foldmethod=indent:foldlevel=1
+
