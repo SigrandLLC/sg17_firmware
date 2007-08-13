62cf1200072654015168bb8c6860f46ca3beafa5
Andrey Kazantsev <kazantsev.av@gmail.com>
2007-08-13 16:03:55 +0000

    QoS initial support
    git-svn-id: http://midge.vlad.org.ua/svn/trunk@812 9f4ba3df-1318-0410-9984-8b9d03078064

---
+++ openwrt-midge/package/webface/files/www/controllers/qos_class_edit.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/haserl
+
+	subsys="network"
+	
+	item=$FORM_item
+	iface=$FORM_iface
+	
+	# calculate next classid identifier
+	if [ "$REQUEST_METHOD" = POST -a -n "$FORM_additem" ]; then
+		max=0
+		for key in `kdb kls sys_iface_${iface}_qos_htb_class_*`; do
+			val=`kdb get $key`
+			eval "$val"
+			cur=`echo $classid |cut -d ':' -f2`
+			if [ $cur -gt $max ]; then
+				max=$cur
+			fi
+		done
+		next=$(($max+1))
+		FORM_classid="1:$next"
+	fi
+
+	# generate list of available classes
+	CLASSES="1:0 root"
+	for key in `kdb kls sys_iface_eth0_qos_htb_class_*`; do
+		val=`kdb get $key`
+		eval "$val"
+		CLASSES="$CLASSES $classid $name"
+	done
+	unset name enabled parent classid rate ceil
+
+	eval_string="export FORM_$item=\"name=$FORM_name enabled=$FORM_enabled parent=$FORM_parent classid=$FORM_classid rate=$FORM_rate ceil=$FORM_ceil\""
+	render_popup_save_stuff
+
+	render_form_header qos_class_edit
+	render_table_title "QoS class edit" 2
+	render_popup_form_stuff
+	
+	render_input_field hidden iface iface "$iface"
+	render_input_field hidden classid classid "$classid"
+	
+	# name
+	desc="Name of class"
+	validator="$tmtreq $validator_rulename"
+	render_input_field text "Short name" name
+	
+	# enabled
+	desc="Check this item to enable rule"
+	validator='tmt:required="true"'
+	render_input_field checkbox "Enable" enabled
+	
+	# parent
+	desc="Name of parent class"
+	render_input_field select "Parent class" parent $CLASSES
+	
+	# rate
+	desc="Class rate"
+	tip="Unit can be: <br/><b>kbit</b>, <b>Mbit</b> - for bit per second<br/> and <b>kbps</b>, <b>Mbps</b> - for bytes per second"
+	validator="$tmtreq $validator_rate"
+	render_input_field text "Rate" rate
+
+	# ceil
+	desc="Maximum rate"
+	tip="Unit can be: <br/><b>kbit</b>, <b>Mbit</b> - for bit per second<br/> and <b>kbps</b>, <b>Mbps</b> - for bytes per second"
+	validator="$tmtreq $validator_rate"
+	render_input_field text "Ceil" ceil
+	
+	render_submit_field
+	render_form_tail
+
+# vim:foldmethod=indent:foldlevel=1
