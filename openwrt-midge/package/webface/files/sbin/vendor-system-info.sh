622fbdf3012e37f401e1ff2b1455bdaa673563b1
Artem Y. Polyakov <artpol84@gmail.com>
2009-05-11 08:22:21 +0000

    Add separate kdb.default for Zelax
    git-svn-id: http://midge.vlad.org.ua/svn/trunk@1343 9f4ba3df-1318-0410-9984-8b9d03078064

--- openwrt-midge/package/webface/files/sbin/vendor-system-info.sh
+++ openwrt-midge/package/webface/files/sbin/vendor-system-info.sh
@@ -3,7 +3,6 @@
 TMP_PATH=/tmp
 TMP_DIR=vendor-sysinfo-reader
 STDIR=$TMP_PATH/$TMP_DIR
-IFNAME=$1
 ARCH_PATH=/root/
 ARCN_NAME=$ARCH_PATH/vendor-sysinfo.tar.bz2
 
@@ -16,13 +15,31 @@ logread > $STDIR/logread.`date -I`.log
 cat /proc/meminfo > $STDIR/meminfo.`date -I`.log
 ps ax > $STDIR/ps.`date -I`.log
 
+# Save information about all interfaces 
+ifconfig > $STDIR/ifconfig.`date -I`.log
+
 # Read SHDSL registers
 dsl_ifs=`ls /sys/class/net/ | grep dsl`
 for i in $dsl_ifs; do
-if [ -d /sys/class/net/$i/sg_private ]; then
-	cat  /sys/class/net/$i/sg_private/regs > $STDIR/$i-regs.`date -I `.log
-fi
+	if [ -d /sys/class/net/$i/sg_private ]; then
+		cat  /sys/class/net/$i/sg_private/regs > $STDIR/$i-regs.`date -I `.log
+	elif [ -d /sys/class/net/$i/sg17_private ]; then
+		cat  /sys/class/net/$i/sg17_private/regs > $STDIR/$i-regs.`date -I `.log
+	fi
 done
+
+
+# Read E1 registers
+dsl_ifs=`ls /sys/class/net/ | grep dsl`
+for i in $dsl_ifs; do
+	if [ -d /sys/class/net/$i/sg_private ]; then
+		cat  /sys/class/net/$i/sg_private/regs > $STDIR/$i-regs.`date -I `.log
+	elif [ -d /sys/class/net/$i/sg17_private ]; then
+		cat  /sys/class/net/$i/sg17_private/regs > $STDIR/$i-regs.`date -I `.log
+	fi
+done
+
+
 
 tar -C $TMP_PATH -cf /root/vendor-sysinfo.tar $TMP_DIR
 echo "/root/vendor-sysinfo.tar created. Send it to Sigrand"
