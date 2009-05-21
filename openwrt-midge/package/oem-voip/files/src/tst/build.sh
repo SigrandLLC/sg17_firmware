df81c033ccf572e6de4cdebe12e43e6ce2facece
Vladimir Luchko <vlad.luch@mail.ru>
2009-05-21 06:15:09 +0000

    VoIP: 	+ Revrited documentation (TF-channels and modules) 	+ Changed some build features (move binaries, not copy)
    git-svn-id: http://midge.vlad.org.ua/svn/trunk@1348 9f4ba3df-1318-0410-9984-8b9d03078064

--- openwrt-midge/package/oem-voip/files/src/tst/build.sh
+++ openwrt-midge/package/oem-voip/files/src/tst/build.sh
@@ -39,10 +39,10 @@ if test -e phone.c; then
 	mipsel-linux-uclibc-gcc -Wall -I$asrc_path/vinetic/include/ -I$asrc_path/tapi/include/ -I$asrc_path/sgatab/ -I$asrc_path -L$asrc_path phone.c -o phone -lab
 fi
 
-echo COPING TO TFTPBOOT...
-	cp *tst ~/tftpboot
+echo MOVING TO TFTPBOOT...
+	mv *tst ~/tftpboot
 	if test -e phone; then
-		cp phone ~/tftpboot
+		mv phone ~/tftpboot
 	fi
 
 cd ${cur_path}/..
