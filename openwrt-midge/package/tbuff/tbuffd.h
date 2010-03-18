d4fb383b4000f2e3413c5670a1080ea37ce6a6ac
Mihail Scherbakov <scherbakov.mihail@gmail.com>
2010-03-18 18:21:09 +0600

    partial merge from master: tbuff: Fixed many bugs...

---
+++ openwrt-midge/package/tbuff/tbuffd.h
@@ -0,0 +1,41 @@
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/select.h>
+
+#include "md5.h"
+#include "kdb.h"
+
+#define SOCKET_NAME "/tmp/socket"
+#define NUM_SOCK 10
+#define MAX_PORTS 16
+#define MAX_DATA 4*1024*1024
+
+struct port
+{
+	int fd;
+	int tbuf;
+	int hbuf;
+	char *buf;
+	struct termios pots;
+	int esc_s;
+	char cmd[100];
+};
