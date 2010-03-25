00e16ab2af801eb1d583f257df3c9ee949250feb
Dmitry Fedorov <dm.fedorov@gmail.com>
2010-03-25 16:07:17 +0600

    rs232-ip: package stuff fixed

--- openwrt-midge/package/depend.mk
+++ openwrt-midge/package/depend.mk
@@ -93,6 +93,7 @@ scanlogd-compile: libpcap-compile libnids-compile libnet-compile
 scdp-compile: libnet-compile
 screen-compile: ncurses-compile
 sftpserver-compile: libiconv-install
+rs232-ip-install: sercd-install socat-install
 sipp-compile: ncurses-compile uclibc++-compile libpthread-compile
 siproxd-compile: libosip2-compile
 sipsak-compile: openssl-compile
