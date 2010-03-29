661fa7cce87610bdf314958301a122b31d649dcc
Dmitry Fedorov <dm.fedorov@gmail.com>
2010-03-29 16:40:36 +0700

    sertest: speed option added

--- openwrt-midge/package/sertest/main.c
+++ openwrt-midge/package/sertest/main.c
@@ -21,7 +21,7 @@
 
 static void usage(void)
 {
-    fprintf(stderr, "Usage: sertest rs232-device\n");
+    fprintf(stderr, "Usage: sertest rs232-device [speed]\n");
     exit(EXIT_FAILURE);
 }
 
@@ -53,7 +53,7 @@ static void restore(void)
     if (fds[RS232].fd >= 0) tcsetattr(fds[RS232].fd, TCSANOW, &tattrs[RS232]);
 }
 
-static void _setmode(enum DeviceIndex i)
+static void _setmode(enum DeviceIndex i, const size_t *speedptr)
 {
     int rc = tcgetattr(fds[i].fd, &tattrs[i]);
     if (rc != 0)
@@ -70,6 +70,56 @@ static void _setmode(enum DeviceIndex i)
     tattr.c_cc[VMIN]  = 1;
     tattr.c_cc[VTIME] = 0;
 
+    if (speedptr != NULL)
+    {
+        speed_t speed;
+
+	switch (*speedptr)
+	{
+	    case 0       : speed=B0      ; break;
+	    case 50      : speed=B50     ; break;
+	    case 75      : speed=B75     ; break;
+	    case 110     : speed=B110    ; break;
+	    case 134     : speed=B134    ; break;
+	    case 150     : speed=B150    ; break;
+	    case 200     : speed=B200    ; break;
+	    case 300     : speed=B300    ; break;
+	    case 600     : speed=B600    ; break;
+	    case 1200    : speed=B1200   ; break;
+	    case 1800    : speed=B1800   ; break;
+	    case 2400    : speed=B2400   ; break;
+	    case 4800    : speed=B4800   ; break;
+	    case 9600    : speed=B9600   ; break;
+	    case 19200   : speed=B19200  ; break;
+	    case 38400   : speed=B38400  ; break;
+	    case 57600   : speed=B57600  ; break;
+	    case 115200  : speed=B115200 ; break;
+	    case 230400  : speed=B230400 ; break;
+	    case 460800  : speed=B460800 ; break;
+	    case 500000  : speed=B500000 ; break;
+	    case 576000  : speed=B576000 ; break;
+	    case 921600  : speed=B921600 ; break;
+	    case 1000000 : speed=B1000000; break;
+	    case 1152000 : speed=B1152000; break;
+	    case 1500000 : speed=B1500000; break;
+	    case 2000000 : speed=B2000000; break;
+	    case 2500000 : speed=B2500000; break;
+	    case 3000000 : speed=B3000000; break;
+	    case 3500000 : speed=B3500000; break;
+	    case 4000000 : speed=B4000000; break;
+	    default:
+		restore();
+		error(EXIT_FAILURE, 0, "%s unknown speed %zu", names[i], *speedptr);
+	}
+
+        rc = cfsetspeed(&tattr, speed);
+	if (rc != 0)
+	{
+	    restore();
+	    error(EXIT_FAILURE, errno, "%s cfsetspeed %zu", names[i], *speedptr);
+	}
+    }
+
     rc = tcsetattr(fds[i].fd, TCSANOW, &tattr);
     if (rc != 0)
     {
@@ -78,16 +128,16 @@ static void _setmode(enum DeviceIndex i)
     }
 }
 
-static void setmode(void)
+static void setmode(const size_t *speedptr)
 {
-    _setmode( INPUT );
-    _setmode( RS232 );
+    _setmode( INPUT, NULL );
+    _setmode( RS232, speedptr );
 }
 
 
 int main(int ac, char *av[])
 {
-    if (ac != 2)
+    if (ac < 2 || ac > 3)
 	usage();
 
     int rc;
@@ -107,8 +157,14 @@ int main(int ac, char *av[])
     if (rc < 1)
 	error(EXIT_FAILURE, 0, "stdin is not a tty");
 
+    if (ac == 3)
+    {
+	size_t speed = atoi(av[2]);
+	setmode( &speed );
+    }
+    else
+	setmode( NULL );
 
-    setmode();
 
     unsigned char old_c = 0;
 
