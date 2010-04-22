217ccd6050c63620f6171902414d399ea5a5d236
Dmitry Fedorov <dm.fedorov@gmail.com>
2010-04-22 20:02:02 +0700

    rs232-ip-extender: network init for server using getaddrinfo

--- openwrt-midge/package/rs232-ip-extender/src/main.c
+++ openwrt-midge/package/rs232-ip-extender/src/main.c
@@ -61,10 +61,64 @@ int main(int ac, char *av[], char *envp[])
 
     int devfd = open_tty(device);
 
-    // getaddrinfo(3)
+    //+ network init
+    //++ server part
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family    = AF_UNSPEC;	// Allow IPv4 or IPv6
+    hints.ai_socktype  = SOCK_STREAM;
+    hints.ai_flags     = AI_PASSIVE;	// For wildcard IP address, INADDR_ANY | IN6ADDR_ANY_INIT
+    hints.ai_protocol  = IPPROTO_TCP;
+    hints.ai_canonname = NULL;
+    hints.ai_addr      = NULL;
+    hints.ai_next      = NULL;
+
+    struct addrinfo *result;
+    rc = getaddrinfo(NULL/*host*/, port, &hints, &result);
+    if (rc != 0)
+    {
+	syslog(LOG_ERR, "getaddrinfo: %s", gai_strerror(rc));
+        fail();
+    }
+
+    /* getaddrinfo() returns a list of address structures.
+       Try each address until we successfully bind(2).
+       If socket(2) (or bind(2)) fails, we (close the socket
+       and) try the next address. */
+
+    struct addrinfo *rp;
+    int sockfd;
+    for (rp = result; rp != NULL; rp = rp->ai_next)
+    {
+	sockfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
+	if (sockfd < 0) continue;
+
+	if (bind(sockfd, rp->ai_addr, rp->ai_addrlen) == 0)
+	    break;                  // Success
+
+	close(sockfd);
+    }
+
+    if (rp == NULL)	// No address succeeded
+    {
+	syslog(LOG_ERR, "Could not bind on %s : %s", host, port);
+        fail();
+    }
+
+    freeaddrinfo(result);	// No longer needed
+
+    rc = listen(sockfd, 2);
+    if (rc < 0)
+    {
+	syslog(LOG_ERR, "listen call error: %m");
+        fail();
+    }
+
+    //-- server part
+    //- network init
+
 
     unlock(device);
     close_tty(devfd);
     return EXIT_SUCCESS;
 }
-
