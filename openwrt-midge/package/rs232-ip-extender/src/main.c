6817796aa8b09b22b9e4485e01d39377a52c9176
Dmitry Fedorov <dm.fedorov@gmail.com>
2010-04-26 18:05:06 +0700

    rs232-ip-extender:main: debug syslog messages added

--- openwrt-midge/package/rs232-ip-extender/src/main.c
+++ openwrt-midge/package/rs232-ip-extender/src/main.c
@@ -79,19 +79,25 @@ int main(int ac, char *av[]/*, char *envp[]*/)
 	socket_t *listen_s = socket_create();
 	socket_bind(listen_s, host, port);
 
+	syslog(LOG_DEBUG, "Waiting data connection...");
 	data_s = socket_accept(listen_s);
 	syslog(LOG_INFO, "Data connection from %s : %s", data_s->host, data_s->port);
 
+	syslog(LOG_DEBUG, "Waiting status connection...");
 	stat_s = socket_accept(listen_s);
 	syslog(LOG_INFO, "Status connection from %s : %s", stat_s->host, stat_s->port);
 
+	syslog(LOG_DEBUG, "Closing listening socket...");
 	socket_close(listen_s);
+	syslog(LOG_DEBUG, "");
     }
     else // connect
     {
+	syslog(LOG_DEBUG, "Connecting (data) to %s:%s ...", host, port);
 	data_s = socket_create();
 	socket_connect(data_s, host, port);
 
+	syslog(LOG_DEBUG, "Connecting (status) to %s:%s ...", host, port);
 	stat_s = socket_create();
 	socket_connect(stat_s, host, port);
     }
