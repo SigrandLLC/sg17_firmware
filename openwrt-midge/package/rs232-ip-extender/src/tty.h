f7541253e26c33f37e71d1a368692195884368f3
Dmitry Fedorov <dm.fedorov@gmail.com>
2010-04-27 21:30:53 +0700

    rs232-ip-extender:tty: i/o methods added

--- openwrt-midge/package/rs232-ip-extender/src/tty.h
+++ openwrt-midge/package/rs232-ip-extender/src/tty.h
@@ -10,17 +10,28 @@ typedef struct
     struct termios termios;
 } tty_t;
 
-tty_t *tty_create (void);
-void   tty_delete (tty_t *t);
+tty_t *tty_create   (void);
+void   tty_delete   (tty_t *t);
 
-void   tty_open   (tty_t *t, const char *devname);
-void   tty_close  (tty_t *t);
+void   tty_open     (tty_t *t, const char *devname);
+void   tty_close    (tty_t *t);
 
-void   tty_set_raw(tty_t *t);
-void   tty_restore(tty_t *t);
+void   tty_set_raw  (tty_t *t);
+void   tty_restore  (tty_t *t);
+
 
 extern inline const char *tty_name(tty_t *t) { return iobase_name(t->b); }
 extern inline int         tty_fd  (tty_t *t) { return iobase_fd  (t->b); }
+
+
+extern inline size_t tty_read     (tty_t *t,       char *buf, size_t len)
+	{ return iobase_read (t->b, buf, len); }
+
+extern inline size_t tty_write    (tty_t *t, const char *buf, size_t len)
+	{ return iobase_write(t->b, buf, len); }
+
+extern inline void   tty_write_all(tty_t *t, const char *buf, size_t len)
+	{        iobase_write(t->b, buf, len); }
 
 
 #endif //RS232_IP_EXTENDER_TTY_H
