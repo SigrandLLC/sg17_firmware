a7494f5b90bd8b5d28279b152cb6b3260e5d63f6
Dmitry Fedorov <dm.fedorov@gmail.com>
2010-04-28 20:26:27 +0700

    rs232-ip-extender:main: restart loop added

--- openwrt-midge/package/rs232-ip-extender/src/main.c
+++ openwrt-midge/package/rs232-ip-extender/src/main.c
@@ -67,126 +67,143 @@ int main(int ac, char *av[]/*, char *envp[]*/)
     setup_sighandler(sig_handler, SIGTERM);
 
 
-    tty_t       *tty = tty_create();
-    tty_open    (tty, device);
-    tty_set_raw (tty);
-
-
     static const size_t   DATA_BUF_SIZE = 4096;
     static const size_t STATUS_BUF_SIZE =  256;
 
     char *  data_buf = xmalloc(  DATA_BUF_SIZE);
     char *status_buf = xmalloc(STATUS_BUF_SIZE);
 
+    socket_t *data_s = NULL, *stat_s = NULL, *listen_s = NULL;
 
-    socket_t *data_s = NULL, *stat_s = NULL;
+    tty_t *tty = tty_create();
 
     if (listen)
     {
-	socket_t *listen_s = socket_create();
-	socket_bind(listen_s, host, port);
-
-	syslog(LOG_INFO, "Waiting data connection...");
-	data_s = socket_accept(listen_s);
-	syslog(LOG_INFO, "Data connection from %s", socket_name(data_s));
-
-	syslog(LOG_INFO, "Waiting status connection...");
-	stat_s = socket_accept(listen_s);
-	syslog(LOG_INFO, "Status connection from %s", socket_name(stat_s));
-
-	syslog(LOG_INFO, "Closing listening socket...");
-	socket_close(listen_s);
+	listen_s = socket_create();
     }
-    else // connect
+    else
     {
-	syslog(LOG_INFO, "Connecting (data) to %s:%s ...", host, port);
 	data_s = socket_create();
-	socket_connect(data_s, host, port);
-
-	syslog(LOG_INFO, "Connecting (status) to %s:%s ...", host, port);
 	stat_s = socket_create();
-	socket_connect(stat_s, host, port);
     }
 
 
-    enum { POLL_TTY, POLL_DATA, POLL_STATUS, POLL_ITEMS };
-
-    struct pollfd polls[POLL_ITEMS];
-    memset(polls, 0, sizeof(polls));
-
-
-    polls[POLL_TTY   ].fd =    tty_fd(tty);
-    polls[POLL_DATA  ].fd = socket_fd(data_s);
-    polls[POLL_STATUS].fd = socket_fd(stat_s);
-
-    int i;
-    for (i = 0; i < POLL_ITEMS; ++i)
-	polls[i].events = POLLIN | POLLRDHUP;
-
-    do
+    do // restart
     {
-	rc = poll(polls, POLL_ITEMS, 5000);
-	if (rc < 0)
-	{
-	    syslog(LOG_ERR, "poll failed: %m");
-	    fail();
-	}
-	else if (rc == 0)	// timeout
-	{
-	    // handle modem lines status
-	    syslog(LOG_INFO, "tick"); //FIXME: TODO
-	}
-	else
-	{
-	    // do i/o
+	tty_open    (tty, device);
+	tty_set_raw (tty);
 
-	    if (polls[POLL_TTY].revents & POLLIN)
+
+	if (listen)
+	{
+	    socket_bind(listen_s, host, port);
+
+	    syslog(LOG_INFO, "Waiting data connection...");
+	    data_s = socket_accept(listen_s);
+	    syslog(LOG_INFO, "Data connection from %s", socket_name(data_s));
+
+	    syslog(LOG_INFO, "Waiting status connection...");
+	    stat_s = socket_accept(listen_s);
+	    syslog(LOG_INFO, "Status connection from %s", socket_name(stat_s));
+
+	    //syslog(LOG_INFO, "Closing listening socket...");
+	    socket_close(listen_s);
+	}
+	else // connect
+	{
+	    syslog(LOG_INFO, "Connecting (data) to %s:%s ...", host, port);
+	    if ( socket_connect(data_s, host, port) )
+                goto cleanup; // restart
+
+	    syslog(LOG_INFO, "Connecting (status) to %s:%s ...", host, port);
+	    if ( socket_connect(stat_s, host, port) )
+                goto cleanup; // restart
+	}
+
+
+	enum { POLL_TTY, POLL_DATA, POLL_STATUS, POLL_ITEMS };
+
+	struct pollfd polls[POLL_ITEMS];
+	memset(polls, 0, sizeof(polls));
+
+
+	polls[POLL_TTY   ].fd =    tty_fd(tty);
+	polls[POLL_DATA  ].fd = socket_fd(data_s);
+	polls[POLL_STATUS].fd = socket_fd(stat_s);
+
+	int i;
+	for (i = 0; i < POLL_ITEMS; ++i)
+	    polls[i].events = POLLIN | POLLRDHUP;
+
+	do
+	{
+	    rc = poll(polls, POLL_ITEMS, 5000);
+	    if (rc < 0)
 	    {
-		size_t r = tty_read(tty, data_buf, DATA_BUF_SIZE);
-		if (r == 0)
+		syslog(LOG_ERR, "poll failed: %m");
+		fail();
+	    }
+	    else if (rc == 0)	// timeout
+	    {
+		// handle modem lines status
+		syslog(LOG_INFO, "tick"); //FIXME: TODO
+	    }
+	    else
+	    {
+		// do i/o
+
+		if (polls[POLL_TTY].revents & POLLIN)
 		{
-		    syslog(LOG_WARNING, "EOF readed from %s, ignore", tty_name(tty));
+		    size_t r = tty_read(tty, data_buf, DATA_BUF_SIZE);
+		    if (r == 0)
+		    {
+			syslog(LOG_WARNING, "EOF readed from %s, ignore", tty_name(tty));
+		    }
+		    else	// r > 0
+		    {
+			socket_send_all(data_s, data_buf, r);
+		    }
 		}
-		else	// r > 0
+
+		if (polls[POLL_DATA].revents & POLLIN)
 		{
-		    socket_send_all(data_s, data_buf, r);
+		    size_t r = socket_recv(data_s, data_buf, DATA_BUF_SIZE);
+		    if (r == 0)
+		    {
+			syslog(LOG_INFO, "data connection: EOF received from %s", socket_name(data_s));
+			break; // restart
+		    }
+		    else
+		    {
+			tty_write_all(tty, data_buf, r);
+		    }
+		}
+
+		if (polls[POLL_STATUS].revents & POLLIN)
+		{
+		    size_t r = socket_recv(stat_s, status_buf, STATUS_BUF_SIZE);
+		    if (r == 0)
+		    {
+			syslog(LOG_INFO, "status connection: EOF received from %s", socket_name(stat_s));
+			break; // restart
+		    }
+		    else
+		    {
+			size_t i;
+			for (i = 0; i < r; ++i)
+			    /*tty_set_lines(tty, status_buf[i])*/;
+		    }
 		}
 	    }
+	} while(1); // poll loop
 
-	    if (polls[POLL_DATA].revents & POLLIN)
-	    {
-		size_t r = socket_recv(data_s, data_buf, DATA_BUF_SIZE);
-		if (r == 0)
-		{
-		    syslog(LOG_INFO, "EOF received from %s", socket_name(data_s));
-		    syslog(LOG_INFO, "closing data connection");
-		    break; // FIXME: restart
-		}
-		else
-		{
-		    tty_write_all(tty, data_buf, r);
-		}
-	    }
-
-	    if (polls[POLL_STATUS].revents & POLLIN)
-	    {
-		size_t r = socket_recv(stat_s, status_buf, STATUS_BUF_SIZE);
-		if (r == 0)
-		{
-		    syslog(LOG_INFO, "EOF received from %s", socket_name(stat_s));
-		    syslog(LOG_INFO, "closing status connection");
-		    break; // FIXME: restart
-		}
-		else
-		{
-		    size_t i;
-		    for (i = 0; i < r; ++i)
-			/*tty_set_lines(tty, status_buf[i])*/;
-		}
-	    }
-	}
-    } while(1);
-
+    cleanup :
+	socket_close(stat_s);
+        socket_close(data_s);
+	tty_close(tty);
+	sleep(1);
+	syslog(LOG_NOTICE, "restart");
+    } while(3); // restart
 
     syslog(LOG_NOTICE, "finished");
     return EXIT_SUCCESS;
