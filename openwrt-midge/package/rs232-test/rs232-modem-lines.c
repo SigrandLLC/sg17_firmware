99bbebf5aa1d4cd879cd6b65e844633a5be0c242
Dmitry Fedorov <dm.fedorov@gmail.com>
2010-05-09 22:42:32 +0700

    rs232-test:rs232-modem-lines: print what doing

--- openwrt-midge/package/rs232-test/rs232-modem-lines.c
+++ openwrt-midge/package/rs232-test/rs232-modem-lines.c
@@ -157,7 +157,7 @@ int main(int ac, char *av[])
 
 	if (rc < 0)
 	    error(EXIT_FAILURE, errno, "poll");
-        else if (rc == 0)
+	else if (rc == 0)
 	    error(EXIT_FAILURE, 0, "poll timeout\n");
 
         if (fds[INPUT].revents & POLLIN)
@@ -173,34 +173,36 @@ int main(int ac, char *av[])
 	    }
 	    else
 	    {
-		printf("Input: '%c' : %d\r\n", c<040?' ':c, c); fflush(stdout);
+		//printf("Input: '%c' : %d\r\n", c<040?' ':c, c); fflush(stdout);
 
 		int mstat;
-                int what;
+		int what;
+		const char *wstr;
 		switch(c)
 		{
-		    case 'z': mstat=TIOCM_LE ; what=TIOCMBIS; break;
-		    case 'Z': mstat=TIOCM_LE ; what=TIOCMBIC; break;
-		    case 'x': mstat=TIOCM_DTR; what=TIOCMBIS; break;
-		    case 'X': mstat=TIOCM_DTR; what=TIOCMBIC; break;
-		    case 'c': mstat=TIOCM_DSR; what=TIOCMBIS; break;
-		    case 'C': mstat=TIOCM_DSR; what=TIOCMBIC; break;
-		    case 'v': mstat=TIOCM_RTS; what=TIOCMBIS; break;
-		    case 'V': mstat=TIOCM_RTS; what=TIOCMBIC; break;
-		    case 'b': mstat=TIOCM_CTS; what=TIOCMBIS; break;
-		    case 'B': mstat=TIOCM_CTS; what=TIOCMBIC; break;
-		    case 'n': mstat=TIOCM_CD ; what=TIOCMBIS; break;
-		    case 'N': mstat=TIOCM_CD ; what=TIOCMBIC; break;
-		    case 'm': mstat=TIOCM_RI ; what=TIOCMBIS; break;
-		    case 'M': mstat=TIOCM_RI ; what=TIOCMBIC; break;
+		    case 'z': mstat=TIOCM_LE ; wstr="set LE "; what=TIOCMBIS; break;
+		    case 'Z': mstat=TIOCM_LE ; wstr="clr LE "; what=TIOCMBIC; break;
+		    case 'x': mstat=TIOCM_DTR; wstr="set DTR"; what=TIOCMBIS; break;
+		    case 'X': mstat=TIOCM_DTR; wstr="clr DTR"; what=TIOCMBIC; break;
+		    case 'c': mstat=TIOCM_DSR; wstr="set DSR"; what=TIOCMBIS; break;
+		    case 'C': mstat=TIOCM_DSR; wstr="clr DSR"; what=TIOCMBIC; break;
+		    case 'v': mstat=TIOCM_RTS; wstr="set RTS"; what=TIOCMBIS; break;
+		    case 'V': mstat=TIOCM_RTS; wstr="clr RTS"; what=TIOCMBIC; break;
+		    case 'b': mstat=TIOCM_CTS; wstr="set CTS"; what=TIOCMBIS; break;
+		    case 'B': mstat=TIOCM_CTS; wstr="clr CTS"; what=TIOCMBIC; break;
+		    case 'n': mstat=TIOCM_CD ; wstr="set CD "; what=TIOCMBIS; break;
+		    case 'N': mstat=TIOCM_CD ; wstr="clr CD "; what=TIOCMBIC; break;
+		    case 'm': mstat=TIOCM_RI ; wstr="set RI "; what=TIOCMBIS; break;
+		    case 'M': mstat=TIOCM_RI ; wstr="clr RI "; what=TIOCMBIC; break;
 		    case  3 :
-			fprintf(stderr, "Input: ^C, exit\r\n");
+			fprintf(stderr, "^C, exit\r\n");
 			exit(EXIT_SUCCESS);
 		    default:
 			printf("Wrong input: '%c' : %d\r\n", c<040?' ':c, c); fflush(stdout);
-                        continue;
+			continue;
 		}
 
+		printf("%s\r\n", wstr);
 		if (ioctl(fds[RS232].fd, what, &mstat) < 0)
 		    error(EXIT_FAILURE, errno, "TIOCMBIS | TIOCMBIC");
 	    }
