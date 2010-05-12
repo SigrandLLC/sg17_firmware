dd50abd75b1fd7844fc5020a384ea693527394c2
Dmitry Fedorov <dm.fedorov@gmail.com>
2010-05-12 18:33:37 +0700

    rs232-tcpext:tty: don't ignore modem control lines again

--- openwrt-midge/package/rs232-tcpext/src/tty.c
+++ openwrt-midge/package/rs232-tcpext/src/tty.c
@@ -142,8 +142,8 @@ int tty_set_raw(tty_t *t)
 
     // not done in cfmakeraw:
     new_tios.c_iflag |=  IGNBRK;	// cleared by cfmakeraw
-    new_tios.c_cflag |=  CLOCAL;	// CLOCAL: Ignore modem control lines
-    new_tios.c_iflag &= ~HUPCL;		// HUPCL: hang up (lower modem control lines) on close?
+    new_tios.c_cflag &= ~CLOCAL;	// !Ignore modem control lines
+    new_tios.c_iflag &= ~HUPCL;		// !hang up (lower modem control lines) on close?
 
 
     if (tcsetattr(tty_fd(t), TCSANOW, &new_tios) < 0)
