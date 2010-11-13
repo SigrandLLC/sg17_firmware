b14181f1eaebf4f1599d88d7292e8bedc1fb583a
Dmitry Fedorov <dm.fedorov@gmail.com>
2010-11-13 21:35:42 +0600

    fix shell scripts for useless use of cat

--- openwrt-midge/package/wf2/files/sbin/vendor-system-info.sh
+++ openwrt-midge/package/wf2/files/sbin/vendor-system-info.sh
@@ -12,19 +12,19 @@ mkdir $STDIR
 # OS information
 dmesg > $STDIR/dmesg.`date -I`.log
 logread > $STDIR/logread.`date -I`.log
-cat /proc/meminfo > $STDIR/meminfo.`date -I`.log
+cp /proc/meminfo $STDIR/meminfo.`date -I`.log
 ps ax > $STDIR/ps.`date -I`.log
 
-# Save information about all interfaces 
+# Save information about all interfaces
 ifconfig > $STDIR/ifconfig.`date -I`.log
 
 # Read SHDSL registers
 dsl_ifs=`ls /sys/class/net/ | grep dsl`
 for i in $dsl_ifs; do
 	if [ -d /sys/class/net/$i/sg_private ]; then
-		cat  /sys/class/net/$i/sg_private/regs > $STDIR/$i-regs.`date -I `.log
-	elif [ -d /sys/class/net/$i/sg17_private ]; then
-		cat  /sys/class/net/$i/sg17_private/regs > $STDIR/$i-regs.`date -I `.log
+		cp /sys/class/net/$i/sg_private/regs   $STDIR/$i-regs.`date -I `.log
+	elif [ -d  /sys/class/net/$i/sg17_private ]; then
+		cp /sys/class/net/$i/sg17_private/regs $STDIR/$i-regs.`date -I `.log
 	fi
 done
 
@@ -33,9 +33,9 @@ done
 dsl_ifs=`ls /sys/class/net/ | grep dsl`
 for i in $dsl_ifs; do
 	if [ -d /sys/class/net/$i/sg_private ]; then
-		cat  /sys/class/net/$i/sg_private/regs > $STDIR/$i-regs.`date -I `.log
-	elif [ -d /sys/class/net/$i/sg17_private ]; then
-		cat  /sys/class/net/$i/sg17_private/regs > $STDIR/$i-regs.`date -I `.log
+		cp /sys/class/net/$i/sg_private/regs   $STDIR/$i-regs.`date -I `.log
+	elif [ -d  /sys/class/net/$i/sg17_private ]; then
+		cp /sys/class/net/$i/sg17_private/regs $STDIR/$i-regs.`date -I `.log
 	fi
 done
 
